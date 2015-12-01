b9978fed835cb4600a12a27de4c1a3f6e75159c6
Jesse Stippel <SkyeStippel@Gmail.com>
2015-12-01 14:53:27 +0100

    added Frontpage & Teaser

--- src/operun/frontpage/content/teaser.py
+++ src/operun/frontpage/content/teaser.py
@@ -9,19 +9,19 @@ from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
-from plone.formwidget.namedfile import NamedBlobFile
+from plone.namedfile.field import NamedBlobImage
 
 
 class ITeaser(Interface):
 
     link = schema.URI(
-        title=_(u'', default='')
-        required=False
+        title=_(u'link', default='Link'),
+        required=False,
     )
 
-    image = NamedBlobFile(
-        title=_(u'', default='')
-        required=False
+    image = NamedBlobImage(
+        title=_(u'image', default='Add Image'),
+        required=False,
     )
 
 
