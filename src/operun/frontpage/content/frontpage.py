d04d6ef52a52a1250fb6603a65703d682195116d
Jesse Stippel <SkyeStippel@Gmail.com>
2015-12-15 11:47:43 +0100

    fixed Flake8 warnings on commit

--- src/operun/frontpage/content/frontpage.py
+++ src/operun/frontpage/content/frontpage.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
-from plone.autoform import directives
+# from plone.autoform import directives
 from plone.dexterity.content import Container
 
 from operun.frontpage import _
 from zope import schema
 from zope.interface import Interface
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+# from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IFrontpage(Interface):
--- src/operun/frontpage/content/teaser.py
+++ src/operun/frontpage/content/teaser.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from plone.autoform import directives
+# from plone.autoform import directives
 from plone.dexterity.content import Item
 
 from operun.frontpage import _
 from zope import schema
 from zope.interface import Interface
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+# from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 from plone.namedfile.field import NamedBlobImage
 
