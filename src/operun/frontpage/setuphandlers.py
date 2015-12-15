53d28bd39bff94b7598310f39978f5bd4efa0bf0
Stefan Antonelli <stefan.antonelli@operun.de>
2015-12-15 15:28:37 +0100

    add frontpage to displayed types

--- src/operun/frontpage/setuphandlers.py
+++ src/operun/frontpage/setuphandlers.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from plone import api
 
 
 @implementer(INonInstallable)
@@ -18,6 +19,7 @@ def post_install(context):
     if context.readDataFile('operunfrontpage_default.txt') is None:
         return
     # Do something during the installation of this package
+    _register_types()
 
 
 def uninstall(context):
@@ -25,3 +27,12 @@ def uninstall(context):
     if context.readDataFile('operunfrontpage_uninstall.txt') is None:
         return
     # Do something during the uninstallation of this package
+
+
+def _register_types():
+    """
+    Add Frontpage to displayed types
+    """
+    displayed_types = api.portal.get_registry_record('plone.displayed_types')
+    displayed_types = displayed_types + (u'Frontpage',)
+    api.portal.set_registry_record('plone.displayed_types', displayed_types)
