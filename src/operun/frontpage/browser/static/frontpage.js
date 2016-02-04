c55347419966f97f51af55c3ce70447d65d41840
Jesse Stippel <SkyeStippel@Gmail.com>
2016-02-04 13:06:20 +0100

    re-compiled

--- src/operun/frontpage/browser/static/frontpage.js
+++ src/operun/frontpage/browser/static/frontpage.js
@@ -11,15 +11,17 @@ require([
   'use strict';
 
   $(document).ready(function() {
-    $('.frontpage-news').each(function() {
-      var highest = 0;
-      $(window).on("resize", function() {
-        $('.js-height').each(function() {
-          if ($(this).height() > highest) {
-            highest = $(this).height();
-          }
+    $(window).on("load resize", function() {
+      $('.frontpage-news').each(function() {
+        var highest = 0;
+        $(window).on("resize", function() {
+          $('.js-height').each(function() {
+            if ($(this).height() > highest) {
+              highest = $(this).height();
+            }
+          });
+          $('.js-height').height(highest);
         });
-        $('.js-height').height(highest);
       });
     });
   });
